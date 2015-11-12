27c9ddb02ea4ec7ffc4db3c8f44970ef9b48888d
drossiter <dan.rossiter@swri.org>
2015-11-12 09:45:19 -0600

    Fixing very broken PGSQL syntax for commlink migration

--- sql/migrate-commlink-max-idle.sql
+++ sql/migrate-commlink-max-idle.sql
@@ -9,37 +9,37 @@ UPDATE iris.comm_link SET idle_secs = 2147483647;
 -- NOTE: Most if not all of the following will be irrelevant to non-CA IRIS. Remove before pushing to MN.
 
 -- all previously per-op commands being updated to per-op equivalent idle_secs
-UPDATE iris.comm_link JOIN iris.comm_protocol ON protocol = id
+UPDATE iris.comm_link
 SET idle_secs = 0
-WHERE iris.comm_protocol.description IN ('Axis PTZ', 'Sensys');
+FROM iris.comm_protocol
+WHERE protocol = id
+AND iris.comm_protocol.description IN ('Axis PTZ', 'Sensys');
 
 
 -- update comm links related to PelcoD protocol
-SET pelcod_conn_mode = (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_pelcod_conn_mode');
-
-UPDATE iris.comm_link JOIN iris.comm_protocol ON protocol = id
-SET idle_secs = 0
-WHERE pelcod_conn_mode = 2 -- PER_OP connection mode
-AND iris.comm_protocol.description = 'Pelco D PTZ';
-
-UPDATE iris.comm_link JOIN iris.comm_protocol ON protocol = id
-SET idle_secs = (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_pelcod_max_idle')
-WHERE pelcod_conn_mode = 1 -- AUTO connection mode
-AND iris.comm_protocol.description = 'Pelco D PTZ';
+UPDATE iris.comm_link
+SET idle_secs = (SELECT CASE
+    WHEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_pelcod_conn_mode') = 1
+      THEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_pelcod_max_idle')
+    WHEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_pelcod_conn_mode') = 2
+      THEN 0
+    ELSE 2147483647
+  END)
+FROM iris.comm_protocol
+WHERE iris.comm_protocol.description = 'Pelco D PTZ';
 
 
 -- update comm links related to Cohu protocol
-SET pelcod_conn_mode = (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_cohu_conn_mode');
-
-UPDATE iris.comm_link JOIN iris.comm_protocol ON protocol = id
-SET idle_secs = 0
-WHERE pelcod_conn_mode = 2 -- PER_OP connection mode
-AND iris.comm_protocol.description = 'Cohu PTZ';
-
-UPDATE iris.comm_link JOIN iris.comm_protocol ON protocol = id
-SET idle_secs = (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_cohu_max_idle')
-WHERE pelcod_conn_mode = 1 -- AUTO connection mode
-AND iris.comm_protocol.description = 'Cohu PTZ';
+UPDATE iris.comm_link
+SET idle_secs = (SELECT CASE
+    WHEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_cohu_conn_mode') = 1
+      THEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_cohu_max_idle')
+    WHEN (SELECT value::INTEGER FROM iris.system_attribute WHERE name = 'camera_cohu_conn_mode') = 2
+      THEN 0
+    ELSE 2147483647
+  END)
+FROM iris.comm_protocol
+WHERE iris.comm_protocol.description = 'Cohu PTZ';
 
 
 -- cleanup unused system attributes
