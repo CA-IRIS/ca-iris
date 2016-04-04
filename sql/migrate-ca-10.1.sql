d1380b40154c8ce19aee169c27e2783a675a148d
Jacob Barde <jacob.barde@swri.org>
2016-04-04 14:06:35 -0500

    sql file fixes for pre-existing data.

--- sql/migrate-ca-10.1.sql
+++ sql/migrate-ca-10.1.sql
@@ -35,9 +35,6 @@ ALTER TABLE iris.map_extent ALTER COLUMN position DROP DEFAULT;
 -- CA trac 504
 INSERT INTO iris.system_attribute(name, value) VALUES('camera_direction_override','');
 
--- CA trac 528
-INSERT INTO iris.comm_protocol (id, description) VALUES(34, 'TTIP DMS');
-
 -- CA trac 476
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_color_high', 'FF0000');
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_color_low', '00FFFF');
@@ -45,12 +42,15 @@ INSERT INTO iris.system_attribute(name, value) VALUES('rwis_color_mid', 'FFC800'
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_high_air_temp_c', 37.0);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_high_precip_rate_mmh', 50);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_high_visibility_distance_m', 300);
-INSERT INTO iris.system_attribute(name, value) VALUES('rwis_high_wind_speed_kph', 40);
+-- value already present
+-- INSERT INTO iris.system_attribute(name, value) VALUES('rwis_high_wind_speed_kph', 40);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_low_air_temp_c', 0.0);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_low_precip_rate_mmh', 5);
-INSERT INTO iris.system_attribute(name, value) VALUES('rwis_low_visibility_distance_m', 152);
+-- value already present
+-- INSERT INTO iris.system_attribute(name, value) VALUES('rwis_low_visibility_distance_m', 152);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_low_wind_speed_kph', 5);
-INSERT INTO iris.system_attribute(name, value) VALUES('rwis_max_valid_wind_speed_kph', 282);
+-- value already present
+-- INSERT INTO iris.system_attribute(name, value) VALUES('rwis_max_valid_wind_speed_kph', 282);
 
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_opacity_percentage', 30);
 INSERT INTO iris.system_attribute(name, value) VALUES('rwis_measurement_radius', 16093.44);
