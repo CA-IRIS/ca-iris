b92305ebefd1e424effc8a8c5f4d5da7275d76f6
Dan Rossiter <dan.rossiter@swri.org>
2016-09-10 13:42:55 -0500

    Adding support for creating a DMS action only specifying a DMS rather than a DMS group.

--- src/us/mn/state/dot/tms/client/schedule/DmsActionModel.java
+++ src/us/mn/state/dot/tms/client/schedule/DmsActionModel.java
@@ -20,9 +20,15 @@ import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.table.TableCellEditor;
+
+import us.mn.state.dot.sonar.client.ProxyListener;
+import us.mn.state.dot.sonar.client.TypeCache;
 import us.mn.state.dot.tms.ActionPlan;
+import us.mn.state.dot.tms.DMS;
+import us.mn.state.dot.tms.DMSHelper;
 import us.mn.state.dot.tms.DmsAction;
 import us.mn.state.dot.tms.DMSMessagePriority;
+import us.mn.state.dot.tms.DmsSignGroup;
 import us.mn.state.dot.tms.PlanPhase;
 import us.mn.state.dot.tms.PlanPhaseHelper;
 import us.mn.state.dot.tms.QuickMessageHelper;
@@ -180,6 +186,47 @@ public class DmsActionModel extends ProxyTableModel<DmsAction> {
 	/** Plan phase model */
 	private final ProxyListModel<PlanPhase> phase_mdl;
 
+    /** Sign group that is in the process of being created. */
+    private String pending_sign_group = null;
+
+    /** DMS for which a sign group is being created. */
+    private DMS pending_dms = null;
+
+	/** DMS sign group type cache */
+	private final TypeCache<DmsSignGroup> dms_sign_groups;
+
+    /** Sign group type cache */
+	private final TypeCache<SignGroup> sign_groups;
+
+	/** Async handling of case where SignGroup must be created */
+	private final ProxyListener<SignGroup> sign_group_listener = new ProxyListener<SignGroup>() {
+
+		@Override
+		public void proxyAdded(SignGroup g) {
+			if (pending_dms != null && g.getName().equals(pending_sign_group)) {
+				String oname = createDmsSignGroupName(pending_dms, g);
+				HashMap<String, Object> attrs = new HashMap<>();
+				attrs.put("dms", pending_dms);
+				attrs.put("sign_group", g);
+				dms_sign_groups.createObject(oname, attrs);
+
+                pending_dms = null;
+                pending_sign_group = null;
+				createObjectCallback(g);
+			}
+		}
+
+		@Override
+		public void enumerationComplete() { }
+
+		@Override
+		public void proxyRemoved(SignGroup proxy) { }
+
+		@Override
+		public void proxyChanged(SignGroup proxy, String a) { }
+
+	};
+
 	/** Create a new DMS action table model */
 	public DmsActionModel(Session s, ActionPlan ap) {
 		super(s, s.getSonarState().getDmsActions(),
@@ -188,6 +235,9 @@ public class DmsActionModel extends ProxyTableModel<DmsAction> {
 		      true);	/* has_name */
 		action_plan = ap;
 		phase_mdl = s.getSonarState().getPhaseModel();
+		dms_sign_groups = s.getSonarState().getDmsCache().getDmsSignGroups();
+		sign_groups = s.getSonarState().getDmsCache().getSignGroups();
+		sign_groups.addProxyListener(sign_group_listener);
 	}
 
 	/** Get the SONAR type name */
@@ -212,10 +262,30 @@ public class DmsActionModel extends ProxyTableModel<DmsAction> {
 	@Override
 	public void createObject(String name) {
 		SignGroup sg = SignGroupHelper.lookup(name.trim());
+		if (sg == null && action_plan != null) {
+			DMS dms = DMSHelper.lookup(name.trim());
+			if (dms != null) {
+				String sg_name = dms.getName();
+
+				// to handle proxy added event later
+				pending_sign_group = sg_name;
+				pending_dms = dms;
+
+				HashMap<String, Object> attrs = new HashMap<String, Object>();
+				attrs.put("local", true);
+				sign_groups.createObject(sg_name, attrs);
+				return;
+			}
+		}
+
+		createObjectCallback(sg);
+	}
+
+	private void createObjectCallback(SignGroup sg) {
 		if (sg != null && action_plan != null)
 			create(sg);
 		else {
-			JOptionPane.showMessageDialog(null, 
+			JOptionPane.showMessageDialog(null,
 				I18N.get("action.plan.dms.hint"));
 		}
 	}
@@ -251,5 +321,17 @@ public class DmsActionModel extends ProxyTableModel<DmsAction> {
 	private PlanPhase lookupPlanPhase() {
 		PlanPhase phase = PlanPhaseHelper.lookup("deployed");
 		return (phase != null) ? phase : action_plan.getDefaultPhase();
+	}
+
+	/** Create a DMS sign group name */
+	private static String createDmsSignGroupName(DMS dms, SignGroup sg) {
+		return sg.getName() + "_" + dms.getName();
+	}
+
+	/** Dispose of the DMS action model */
+	@Override
+	public void dispose() {
+		super.dispose();
+		sign_groups.removeProxyListener(sign_group_listener);
 	}
 }
