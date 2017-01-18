5964dc04a45e00dd26be334aff50a5250062a5bd
Jacob Barde <jacob.barde@swri.org>
2017-01-18 17:48:01 -0600

    added caltrans 10.3 migration sql script

---
+++ sql/caltrans/migrate-ca-10.3.sql
@@ -0,0 +1,27 @@
+-- current as of MnDOT 4.35.4
+
+
+-- updates required before rest of updates
+\set ON_ERROR_STOP
+
+SET SESSION AUTHORIZATION 'tms';
+
+-- updates CA-only protocols (that haven't been given to MnDOT)
+
+-- ADDITIONS for CA-IRIS v10.2 go at the bottom
+
+
+
+
+-- ============================================================================
+-- BEGIN: MnDOT updates
+-- ============================================================================
+
+
+
+-- ============================================================================
+-- BEGIN 10.2 changes
+-- ============================================================================
+
+
+
