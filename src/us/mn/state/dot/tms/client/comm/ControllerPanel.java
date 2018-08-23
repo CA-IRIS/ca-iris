862c8e1126532a2d07eb9f35beb83dc57eac3a9a
Wesley Skillern <wesley.skillern@swri.org>
2018-08-23 16:05:11 -0500

    - JComboBox search debugged

--- src/us/mn/state/dot/tms/client/comm/ControllerPanel.java
+++ src/us/mn/state/dot/tms/client/comm/ControllerPanel.java
@@ -16,6 +16,7 @@ package us.mn.state.dot.tms.client.comm;
 
 import us.mn.state.dot.tms.CommLink;
 import us.mn.state.dot.tms.Controller;
+import us.mn.state.dot.tms.ControllerIO;
 import us.mn.state.dot.tms.CtrlCondition;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.proxy.ProxyTablePanel;
@@ -24,10 +25,7 @@ import us.mn.state.dot.tms.client.widget.IAction;
 import us.mn.state.dot.tms.client.widget.ILabel;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 import static java.awt.event.KeyEvent.*;
 import static us.mn.state.dot.tms.client.comm.ControllerIOModel.IO_TYPE;
@@ -105,23 +103,52 @@ public class ControllerPanel extends ProxyTablePanel<Controller> {
 	private final ILabel dev_name_label = new ILabel(
 		"device.search");
 
-	/** a text field to hold the Controller IO search text */
-	private final JComboBox dev_name_cbx;
+	/** a text field to hold the Controller IO name search */
+	private final JComboBox<ControllerIO> dev_name_cbx;
+
+	/**
+	 * Handles selected items from the dev_name_cbx drop down.
+	 */
 	private ActionListener dev_name_act = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			switch (e.getActionCommand()) {
 			case "comboBoxChanged":
 				if (!updatingDeviceList) {
-					dev_name_cbx.hidePopup();
 					String item = (String)
 						dev_name_cbx.getSelectedItem();
 					setDeviceSearch(item);
+					dev_name_cbx.getEditor().setItem(item);
+					if (item == null || item.isEmpty())
+						updateDeviceList();
 				}
 				break;
 			case "comboBoxEdited":
-				dev_name_cbx.hidePopup();
+//				dev_name_cbx.hidePopup();
 				break;
+			}
+		}
+	};
+
+	/**
+	 * Handles search text in the dev_name_cbx
+	 */
+	private KeyListener dev_name_key_act = new KeyAdapter() {
+		@Override
+		public void keyReleased(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case VK_UP:
+			case VK_DOWN:
+			case VK_LEFT:
+			case VK_RIGHT:
+			case VK_ENTER:
+				break;
+			default:
+				dev_name_cbx.showPopup();
+				String s = (String)
+					dev_name_cbx.getEditor().getItem();
+				setDeviceSearch(s);
+				updateDeviceList();
 			}
 		}
 	};
@@ -129,7 +156,8 @@ public class ControllerPanel extends ProxyTablePanel<Controller> {
 	/** Create a new controller panel */
 	public ControllerPanel(Session s) {
 		super(new ControllerTableModel(s));
-		dev_name_cbx = new JComboBox(((ControllerTableModel) model).getMatchedDevices().toArray());
+		dev_name_cbx = new JComboBox(((ControllerTableModel) model)
+			.getMatchedDevices().toArray());
 	}
 
 	/** Initialize the panel */
@@ -142,44 +170,24 @@ public class ControllerPanel extends ProxyTablePanel<Controller> {
 		dev_type_cbx.setAction(dev_type_act);
 		dev_name_cbx.setEditable(true);
 		dev_name_cbx.addActionListener(dev_name_act);
-		dev_name_cbx.getEditor().getEditorComponent().addKeyListener(
-			new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				switch (e.getKeyCode()) {
-				case VK_UP:
-				case VK_DOWN:
-				case VK_LEFT:
-				case VK_RIGHT:
-					break;
-				default:
-					dev_name_cbx.showPopup();
-					updateDeviceList();
-				}
-			}
-		});
+		dev_name_cbx.getEditor().getEditorComponent().addKeyListener(dev_name_key_act);
 	}
 
 	private boolean updatingDeviceList = false;
 
+	/**
+	 * updates the the device list in dev_name_cbx
+	 */
 	private void updateDeviceList() {
 		updatingDeviceList = true;
 		boolean visibility = dev_name_cbx.isPopupVisible();
 		dev_name_cbx.setPopupVisible(false);
 		String s = (String) dev_name_cbx.getEditor().getItem();
-		setDeviceSearch(s);
 		dev_name_cbx.removeAllItems();
 		dev_name_cbx.setModel(new DefaultComboBoxModel(((ControllerTableModel) model).getMatchedDevices().toArray()));
 		dev_name_cbx.getEditor().setItem(s);
 		dev_name_cbx.setPopupVisible(visibility);
 		updatingDeviceList = false;
-	}
-
-	private void status(String s) {
-		System.out.println(s + " \n" +
-			"  search: " + ((ControllerTableModel) model).getDevSearch() + "\n" +
-			"  editor text: " + dev_name_cbx.getEditor().getItem() + "\n" +
-			"  selected item: " + dev_name_cbx.getSelectedItem());
 	}
 
 	/** Add create/delete widgets to the button panel */
