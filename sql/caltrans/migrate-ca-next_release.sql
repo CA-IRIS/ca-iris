30051d9a9f4fab9d0a6a5a896e0675916982a7e3
Wesley Don Skillern <wesley.skillern@swri.org>
2020-01-06 15:58:47 -0600

    minor version bump to 10.4.5

--- sql/caltrans/migrate-ca-next_release.sql
+++ sql/caltrans/migrate-ca-next_release.sql
@@ -7,7 +7,7 @@ SET SESSION AUTHORIZATION 'tms';
 INSERT INTO iris.system_attribute (name, value) VALUES ('dms_querymsg_after_send_new_msg', false);
 
 UPDATE iris.system_attribute
-    SET value = regexp_replace(value, '10\.4\.\d+.*\s+\[', '10.4.4 [')
+    SET value = regexp_replace(value, '10\.4\.\d+.*\s+\[', '10.4.5 [')
     WHERE NAME = 'window_title';
 
 -- CAIRISINT48/TT623
