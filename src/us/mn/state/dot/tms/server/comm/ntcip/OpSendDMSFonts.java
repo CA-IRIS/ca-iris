cf0e3726b380b06e4c3613a653493ee112eb5323
Sriram Bhat <sriram.bhat@swri.org>
2020-09-17 12:51:40 -0500

    "Font 1 aborted - Expected noUsed, was notUsedReq, Font 17 abor" error when Send Settings button clicked (This bug fix specifically targets the Daktronics sign).

--- src/us/mn/state/dot/tms/server/comm/ntcip/OpSendDMSFonts.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/OpSendDMSFonts.java
@@ -55,6 +55,9 @@ public class OpSendDMSFonts extends OpDMS {
 			fontStatus.node, row);
 	}
 
+	/** Time in seconds to allow for verifying font status */
+	static private final int VERIFY_STATUS_SECS = 5;
+
 	/** Number of fonts supported */
 	private final ASN1Integer num_fonts = numFonts.makeInt();
 
@@ -332,6 +335,10 @@ public class OpSendDMSFonts extends OpDMS {
 	/** Phase to verify the font status is "notUsed" */
 	protected class VerifyStatusNotUsed extends Phase {
 
+		/** Time to stop checking if the status has updated */
+		private final long expire = TimeSteward.currentTimeMillis() +
+				VERIFY_STATUS_SECS * 1000;
+
 		/** Verify the font status is "notUsed" */
 		@SuppressWarnings("unchecked")
 		protected Phase poll(CommMessage mess) throws IOException {
@@ -339,6 +346,12 @@ public class OpSendDMSFonts extends OpDMS {
 			mess.add(status);
 			mess.queryProps();
 			logQuery(status);
+			if (status.getEnum() == FontStatus.notUsedReq) {
+				// Daktronics DMS return notUsedReq instead of
+				// notUsed for a short time; try again
+				if (TimeSteward.currentTimeMillis() < expire)
+					return this;
+			}
 			if (status.getEnum() != FontStatus.notUsed) {
 				abortUpload("Expected notUsed, was "
 					+ status.getEnum());
@@ -366,6 +379,10 @@ public class OpSendDMSFonts extends OpDMS {
 	/** Phase to verify the font status is modifying */
 	protected class VerifyStatusModifying extends Phase {
 
+		/** Time to stop checking if the status has updated */
+		private final long expire = TimeSteward.currentTimeMillis() +
+				VERIFY_STATUS_SECS * 1000;
+
 		/** Verify the font status is modifying */
 		@SuppressWarnings("unchecked")
 		protected Phase poll(CommMessage mess) throws IOException {
@@ -373,6 +390,12 @@ public class OpSendDMSFonts extends OpDMS {
 			mess.add(status);
 			mess.queryProps();
 			logQuery(status);
+			if (status.getEnum() == FontStatus.modifyReq) {
+				// Daktronics DMS return modifyReq instead of
+				// modifying for a short time; try again
+				if (TimeSteward.currentTimeMillis() < expire)
+					return this;
+			}
 			if (status.getEnum() != FontStatus.modifying) {
 				abortUpload("Expected modifying, was " +
 					status.getEnum());
@@ -532,7 +555,7 @@ public class OpSendDMSFonts extends OpDMS {
 		static private final int CALCULATING_ID_SECS = 15;
 
 		/** Time to stop checking if the font is ready for use */
-		private final long expire = TimeSteward.currentTimeMillis() + 
+		private final long expire = TimeSteward.currentTimeMillis() +
 			CALCULATING_ID_SECS * 1000;
 
 		/** Verify the font status is ready for use */
@@ -548,6 +571,9 @@ public class OpSendDMSFonts extends OpDMS {
 					return new SetDefaultFont();
 				else
 					return nextFontPhase();
+			case readyForUseReq:
+				// Daktronics DMS return readyForUseReq instead
+				// of calculatingID for a short time; try again
 			case calculatingID:
 				if (TimeSteward.currentTimeMillis() > expire) {
 					abortUpload("Still calculatingID, " +
