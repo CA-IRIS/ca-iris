b3827d64893e852f1c0400ed226fe5fc7026c416
Sriram Bhat <sriram.bhat@swri.org>
2020-11-09 18:29:55 -0600

    Updated logic for OpQueryLCSMessage to address blank messages, messages that are configured as LaneUseMultis for a DMS, and external messages (not permanent or blank) from other possible vendors

--- src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryLCSMessage.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryLCSMessage.java
@@ -1,9 +1,10 @@
 package us.mn.state.dot.tms.server.comm.ntcip;
 
 import us.mn.state.dot.tms.DMSMessagePriority;
+import us.mn.state.dot.tms.InvalidMessageException;
+import us.mn.state.dot.tms.SignMessage;
 import us.mn.state.dot.tms.server.DMSImpl;
 import us.mn.state.dot.tms.server.comm.CommMessage;
-import us.mn.state.dot.tms.server.comm.ntcip.mib1203.DmsMessageCRC;
 import us.mn.state.dot.tms.server.comm.ntcip.mib1203.DmsMessageMemoryType;
 import us.mn.state.dot.tms.server.comm.ntcip.mib1203.DmsMessageStatus;
 import us.mn.state.dot.tms.server.comm.snmp.ASN1Enum;
@@ -12,6 +13,7 @@ import us.mn.state.dot.tms.server.comm.snmp.ASN1String;
 
 import java.io.IOException;
 
+import static us.mn.state.dot.tms.server.comm.ntcip.mib1203.MIB1203.dmsMessageBeacon;
 import static us.mn.state.dot.tms.server.comm.ntcip.mib1203.MIB1203.dmsMessageMultiString;
 import static us.mn.state.dot.tms.server.comm.ntcip.mib1203.MIB1203.dmsMessageRunTimePriority;
 import static us.mn.state.dot.tms.server.comm.ntcip.mib1203.MIB1203.dmsMessageStatus;
@@ -39,7 +41,9 @@ public class OpQueryLCSMessage extends OpQueryDMSMessage {
              * signs use 'undefined' source for blank messages. */
             if (mem_type.isBlank())
                 return processMessageBlank();
-            else if (mem_type.valid)
+            else if (mem_type.valid) {
+                return processMessageValid();
+            } else
                 return new QueryCurrentMsgPrior();
         }
         return processMessageInvalid();
@@ -76,32 +80,94 @@ public class OpQueryLCSMessage extends OpQueryDMSMessage {
             mess.add(time);
             mess.queryProps();
             logQuery(time);
-            return processMessageValid();
+            return new QueryMsgMultiString(); // look at OpQueryDMSMessage as reference
+        }
+    }
+
+    private ASN1String lcsMulti;
+
+    /** Phase to query the message MULTI string*/
+    protected class QueryMsgMultiString extends Phase {
+
+        /** Query the message MULTI string */
+        @SuppressWarnings("unchecked")
+        protected Phase poll(CommMessage mess) throws IOException {
+            ASN1String ms = new ASN1String(dmsMessageMultiString
+                    .node, DmsMessageMemoryType.currentBuffer
+                    .ordinal(), 1);
+            mess.add(ms);
+            mess.queryProps();
+            lcsMulti = ms;
+            logQuery(ms);
+            return new QueryMsgBeacon();
+        }
+    }
+
+    private ASN1Integer lcsBeacon;
+
+    /** Phase to query the message beacon*/
+    protected class QueryMsgBeacon extends Phase {
+
+        /** Query the message beacon */
+        @SuppressWarnings("unchecked")
+        protected Phase poll(CommMessage mess) throws IOException {
+            ASN1Integer beacon = dmsMessageBeacon.makeInt(
+            DmsMessageMemoryType.currentBuffer, 1);
+            mess.add(beacon);
+            mess.queryProps();
+            lcsBeacon = beacon;
+            logQuery(beacon);
+            return new QueryMsgStatus();
+        }
+    }
+
+    private ASN1Enum<DmsMessageStatus> lcsStatus;
+
+    /** Phase to query the message status*/
+    protected class QueryMsgStatus extends Phase {
+
+        /** Query the message status */
+        @SuppressWarnings("unchecked")
+        protected Phase poll(CommMessage mess) throws IOException {
+            ASN1Enum<DmsMessageStatus> status = new ASN1Enum<
+            DmsMessageStatus>(DmsMessageStatus.class,
+            dmsMessageStatus.node,
+            DmsMessageMemoryType.currentBuffer.ordinal(),1);
+            mess.add(status);
+            mess.queryProps();
+            lcsStatus = status;
+            logQuery(status);
+            setCurrentMessage();
+            return null;
         }
     }
 
     @Override
     protected Phase processMessageValid() throws IOException {
         if (source.getMemoryType() == DmsMessageMemoryType.permanent) {
-            String multi = findLaneUseMultiIndication(source.getNumber()).
-                            getQuickMessage().getMulti();
-            int msg_num = findLaneUseMultiIndication(source.getNumber()).getMsgNum();
-            if (source.getNumber() == msg_num)
-                setCurrentMessage();
-            else
-                source.setMemoryType(DmsMessageMemoryType.undefined);
+            try {
+                SignMessage sm = dms.getMessageCurrent();
+                int msg_num = findDmsLaneUseMulti(sm.getMulti()).getMsgNum();
+                if (source.getNumber() == msg_num)
+                    setCurrentMessage(sm);
+                else
+                    return new QueryCurrentMsgPrior();
+            } catch (InvalidMessageException e) {
+                e.printStackTrace();
+            }
         } else {
-            logError("INVALID SOURCE");
-            setErrorStatus(source.toString());
+            return new QueryCurrentMsgPrior();
         }
         return null;
     }
 
     private void setCurrentMessage() throws IOException {
         Integer d = parseDuration(lcsTime.getInteger());
-        DMSMessagePriority prior = DMSMessagePriority.SCHEDULED;
-        String multi = findLaneUseMultiIndication(source.getNumber()).
-                getQuickMessage().getMulti();
-        setCurrentMessage(multi, 0, prior, d);
+        DMSMessagePriority rp = lcsPrior.getEnum();
+        /* If it's null, IRIS didn't send it ... */
+        if (rp == null)
+            rp = DMSMessagePriority.OTHER_SYSTEM;
+        setCurrentMessage(lcsMulti.getValue(),
+                lcsBeacon.getInteger(), rp, d);
     }
 }
